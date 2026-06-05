1f460aaea6ebeeb4a53e6abbe0976b2cee979a21
kartofanushka <140699782+kartofanushka@users.noreply.github.com>
2026-06-06 00:49:13 +0700

    lineFollowingRobot.ino

--- lineFollowingRobot.ino
+++ lineFollowingRobot.ino
@@ -224,7 +224,7 @@ void payload() {
 long microsecondsToCentimeters(long microseconds) {
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
   // The ping travels out and back, so to find the distance of the object we
-  // take half of the distance travelled.
+  // took half of the distance travelled.
   return microseconds / 29 / 2;
 }
 
